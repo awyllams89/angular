3f474d9aed0a4981e77b7cbc1cbbe8750573a798
awyllams89 <awyllams89@gmail.com>
2024-11-20 08:19:34 -0300

    Cria o navbar

--- forms/src/app/app-routing.module.ts
+++ forms/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { DataFormComponent } from './data-form/data-form.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'templateForm', component:TemplateFormComponent},
+  {path:'dataForm', component:DataFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
